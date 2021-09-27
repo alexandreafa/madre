e80601f53db74f2d5438bbe622126ce86a4944ec
Alexandre Felipe <alexandre.f@academico.ifg.edu.br>
2021-09-27 12:48:10 -0300

    adicionar filtragem de exame por grupo - BASIS-227321

--- exames/src/main/java/br/com/basis/madre/madreexames/repository/ExameRepository.java
+++ exames/src/main/java/br/com/basis/madre/madreexames/repository/ExameRepository.java
@@ -4,10 +4,14 @@ import br.com.basis.madre.madreexames.domain.Exame;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Spring Data  repository for the Exame entity.
  */
 @SuppressWarnings("unused")
 @Repository
 public interface ExameRepository extends JpaRepository<Exame, Long> {
+
+    List<Exame> findByGrupoAgendamentoExamesId(Long id);
 }
